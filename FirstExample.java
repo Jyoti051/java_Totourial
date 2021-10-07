d2737d92105434b12f1d11e7834e1286c7f3949c
Jyoti051 <92112744+Jyoti051@users.noreply.github.com>
2021-10-07 22:21:36 +0530

    Update FirstExample.java

--- FirstExample.java
+++ FirstExample.java
@@ -1,9 +1,13 @@
 class FirstExample
 {
- 
+ static void print()
+ {
+  System.out.println("Hello world");
+ }
   public static void main(String[] args)
   {
    System.out.println("hello everyone"); //this is how we print messages on consol
+   print();// this is how we call static method
   }
   
 }
