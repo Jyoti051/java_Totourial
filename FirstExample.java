0afee91c3ad19f71108b82c115ae0f88552b2cfe
Jyoti051 <92112744+Jyoti051@users.noreply.github.com>
2021-10-07 22:24:48 +0530

    Update FirstExample.java

--- FirstExample.java
+++ FirstExample.java
@@ -2,7 +2,7 @@ class FirstExample
 {
  static void print()
  {
-  System.out.println("Hello world");
+  System.out.println("Hello world");//static method
  }
   public static void main(String[] args)
   {
